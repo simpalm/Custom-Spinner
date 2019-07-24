3cb9e6f207607d6bebd296599078c10c2c9b069a
Aman Sinha <aman@simpalm.com>
2019-07-24 21:37:38 +0530

    Updating Framework for Swift compiler 5.0

---
+++ Framework/CustomSimpalmSpinner.framework/Headers/CustomSimpalmSpinner.h
@@ -0,0 +1,19 @@
+//
+//  CustomSimpalmSpinner.h
+//  CustomSimpalmSpinner
+//
+//  Created by Aman Sinha on 24/07/19.
+//  Copyright © 2019 Simpalm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CustomSimpalmSpinner.
+FOUNDATION_EXPORT double CustomSimpalmSpinnerVersionNumber;
+
+//! Project version string for CustomSimpalmSpinner.
+FOUNDATION_EXPORT const unsigned char CustomSimpalmSpinnerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomSimpalmSpinner/PublicHeader.h>
+
+
